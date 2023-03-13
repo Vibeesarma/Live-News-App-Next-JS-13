72be51b1f16d09c834c9ba521260f245b33538a3
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-03-13 19:24:16 +0000

    queries works fine

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import fetchNews from "../lib/fetchNews";
 
 const HomePage = async () => {
   const news: NewsResponse = await fetchNews(categories.join(","));
+  console.log("🚀 ~ file: page.tsx:6 ~ HomePage ~ NewsResponse:", news)
 
   return <div>{/* News list */}</div>;
 };
--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,4 +1,5 @@
 import { gql } from "graphql-request";
+import sortNewsByImage from "./sortNewsByImage";
 
 const fetchNews = async (
   category?: Category | string,
@@ -6,19 +7,19 @@ const fetchNews = async (
   isDynamic?: boolean
 ) => {
   // graphql query
-  const GET_QUERY = gql`
+  const query = gql`
     query MyQuery(
-      $access_key:String!
-      $categories:String!
-      $keywords:String
-    ){
+      $access_key: String!
+      $categories: String!
+      $keywords: String
+    ) {
       myQuery(
         access_key: $access_key
-        categories:$categories
-        countries:"gb"
-        sort:"published_desc"
-        keywords:$keywords
-        ) {
+        courtries: "gb"
+        sources: $categories
+        sort: "published_desc"
+        keywords: $keywords
+      ) {
         data {
           author
           category
@@ -41,9 +42,36 @@ const fetchNews = async (
     }
   `;
 
-  // fetch function with nextjs13 cache
+  // fetch function with nextjs13 caching...........
+  const res = await fetch(
+    "https://guapua.stepzen.net/api/exiled-elephant/__graphql",
+    {
+      method: "POST",
+      cache: isDynamic ? "no-cache" : "default",
+      // the below one mean if not dynminc it's vslidate the response and fetch only after 20s
+      next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
+      },
+      body: JSON.stringify({
+        query,
+        variables: {
+          access_key: process.env.MEDIASTACK_API_KEY,
+          categories: category,
+          keywords: keywords,
+        },
+      }),
+    }
+  );
+
+  const newsResponse = await res.json();
+
   // sort function by images vs not images present
+  const news = sortNewsByImage(newsResponse.data.myQuery);
+
   // return res
+  return news;
 };
 
 export default fetchNews;
